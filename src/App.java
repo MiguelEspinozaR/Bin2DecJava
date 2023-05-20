0377d9da09bf624f01a847c6fa9be0bbcb9c7ff7
Miguel Espinoza R <mespinozar001@gmail.com>
2023-05-20 03:58:45 -0400

    me ganó el sueño, longitud de entrada implementado. Continuaremos con la conversión

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int input = 0;
+        System.out.println("Hello :) \n\nI will convert your binary number to decimal");
+        System.out.println("input up to 8 bits: \n");
+        while(true){
+            input = sc.nextInt();
+            if(input <= 11111111){
+                break;
+            } else {
+                System.out.println("Debe tener una longitud de 8 caracteres o menos :T\nIntenta de nuevo:");
+            }
+        }
+
+        int output = 0;
+        int aux = 0;
+        for (int i = 0; input > 0; i++) {
+            aux = input % 10;
+            input = input/10;
+             
+        }
+
     }
 }
